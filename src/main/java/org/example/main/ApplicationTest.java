f6135b9e4e98095a66e7f32f5aa2bf9c0063b110
kanthi-sree765 <gkanthisreeyadav@gmail.com>
2025-11-23 15:51:29 +0530

    Pushed test file

---
+++ src/main/java/org/example/main/ApplicationTest.java
@@ -0,0 +1,12 @@
+package org.example.main;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ApplicationTest {
+
+    @Test
+    void sampleTest() {
+        assertTrue(true);
+    }
+}
